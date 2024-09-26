a46d1775b48f689eb0e3680b7431799226c31072
Tan Hoang <tan.hoang@sotatek.com>
2024-09-26 15:42:33 +0700

    feat: unit tests for crawl token

---
+++ src/modules/crawler/tests/crawl-token.spec.ts
@@ -0,0 +1,63 @@
+import { ConfigModule } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { ConfigurationModule } from '../../../config/config.module.js';
+import { EAsset } from '../../../constants/api.constant.js';
+import { TokenPriceRepository } from '../../../database/repositories/token-price.repository.js';
+import { LoggerService } from '../../../shared/modules/logger/logger.service.js';
+import { BatchJobGetPriceToken } from '../batch.tokenprice.js';
+
+describe('BlockchainEVMCraler', () => {
+  let tokenCrawlService: BatchJobGetPriceToken;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
+        ConfigurationModule,
+      ],
+      providers: [
+        BatchJobGetPriceToken,
+        {
+          provide: TokenPriceRepository,
+          useValue: {
+            getTokenPriceBySymbol: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+        {
+          provide: LoggerService,
+          useValue: {
+            getLogger: jest.fn().mockReturnValue({
+              info: jest.fn(),
+              warn: jest.fn(),
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    tokenCrawlService = module.get<BatchJobGetPriceToken>(BatchJobGetPriceToken);
+  });
+
+  describe('handle crawl token price', () => {
+    it('should update the token price', async () => {
+      const result1 = await tokenCrawlService.updateTokenPrice(EAsset.ETH, '1.1');
+      const result2 = await tokenCrawlService.updateTokenPrice(EAsset.ETH, 1.1 as any);
+
+      expect(result1).toBeTruthy();
+      expect(result2).toBeTruthy();
+    });
+    it('should fail to update the token price', async () => {
+      const result1 = await tokenCrawlService.updateTokenPrice(EAsset.ETH, null);
+      const result2 = await tokenCrawlService.updateTokenPrice(EAsset.ETH, undefined);
+      const result3 = await tokenCrawlService.updateTokenPrice(EAsset.ETH, '');
+
+      expect(result1).toBeFalsy();
+      expect(result2).toBeFalsy();
+      expect(result3).toBeFalsy();
+    });
+  });
+});
